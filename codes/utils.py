ab81b1b5f16e063ccfe059dafccbb3dddff4a84a
Yahui Liu <yahui.cvrs@gmail.com>
2024-12-24 17:10:43 +0800

    update

--- codes/utils.py
+++ codes/utils.py
@@ -1,5 +1,6 @@
 import torch
 
+# greedy search
 def generate_text_simple(model, idx, max_new_tokens, context_size):
     for _ in range(max_new_tokens):
         idx_second = idx[:, -context_size:]
@@ -11,6 +12,32 @@ def generate_text_simple(model, idx, max_new_tokens, context_size):
         idx_next = torch.argmax(probs, dim=-1, keepdim=True)
 
         idx = torch.cat([idx, idx_next], dim=1)
+    return idx
+
+# probabilistic sampling + temperature scaling -> top-k sampling
+def generate(model, idx, max_new_tokens, context_size, 
+    temperature=0.0, top_k=None, eos_id=None):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:,-context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+        logits = logits[:, -1, :]
+
+        if top_k is not None:
+            top_logits, _ = torch.topk(logits, top_k)
+            min_val = top_logits[:, -1]
+            logits = torch.where(logits < min_val, 
+                                 torch.tensor(-torch.inf).to(logits.device), 
+                                 logits)
+
+        if temperature > 0.0:
+            logits /= temperature
+            probs = torch.softmax(logits, dim=-1)
+            idx_next = torch.multinomial(probs, num_samples=1)
+        else:
+            idx_next = torch.argmax(logits, dim=-1, keepdim=True)
+
+        idx = torch.cat([idx, idx_next], dim=-1)
     return idx
 
 
